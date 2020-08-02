57ffa32d8c37c6bb03acdde8eb2fa43f73e04587
Devin <devinpquinn4@gmail.com>
2020-08-02 02:43:36 -0400

    improved witchlight mechanic

--- Assets/Scripts/Witchlight.cs
+++ Assets/Scripts/Witchlight.cs
@@ -6,13 +6,16 @@ public class Witchlight : MonoBehaviour
 {
     private bool inFlight = false;
     private Transform parentPlayer;
-    private float flySpeed = 6f;
+    private float flySpeed = 10f;
     private Rigidbody rb;
+    public Vector3 offset;
 
     private void Start()
     {
         parentPlayer = Camera.main.gameObject.transform;
         transform.parent = parentPlayer;
+        transform.localPosition = offset;
+        transform.localRotation = Quaternion.identity;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -21,20 +24,25 @@ public class Witchlight : MonoBehaviour
         if(Input.GetMouseButtonDown(0) && !inFlight)
         {
             transform.parent = null;
-            rb.velocity = parentPlayer.forward * flySpeed;
+            Vector3 facing = parentPlayer.forward;
+            rb.velocity = facing * flySpeed;
             inFlight = true;
         }
         if (Input.GetMouseButtonDown(1) && inFlight)
         {
             rb.velocity = new Vector3(0f, 0f, 0f);
             transform.parent = parentPlayer;
-            transform.localPosition = new Vector3(0f, 0f, 0f);
+            transform.localPosition = offset;
+            transform.localRotation = Quaternion.identity;
             inFlight = false;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        rb.velocity = new Vector3(0f, 0f, 0f);
+        if(!other.CompareTag("Player"))
+        {
+            rb.velocity = new Vector3(0f, 0f, 0f);
+        }
     }
 }
