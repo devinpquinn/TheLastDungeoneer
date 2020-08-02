0e26038f190ddf3e0ddb15ee0b5dcf7fec96c9ff
Devin <devinpquinn4@gmail.com>
2020-08-02 00:57:21 -0400

    added particle system scaling script

---
+++ Assets/Scripts/DustScaler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustScaler : MonoBehaviour
+{
+    private void Start()
+    {
+        ParticleSystem p = GetComponent<ParticleSystem>();
+        var em = p.emission;
+        var s = p.shape.box;
+        em.rateOverTime = ((s.x * s.y * s.z) / 5f);
+    }
+}
